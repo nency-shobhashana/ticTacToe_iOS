0ade8ae3d6667737f1049441bba08e57f3bcf385
Nency <nency@Nencys-Mac.local>
2021-01-20 16:16:04 -0500

    added actual game code

--- TicTacToe-template/ViewController.swift
+++ TicTacToe-template/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    var activePlayer = 1
+    var activeGame = true
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    let winnigCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnPlayAgain.isHidden = true
     }
 
-
+    @IBAction func playBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clickSquareBtn(_ sender: UIButton) {
+//        print(sender.tag)
+        let activePosition = sender.tag - 1
+    }
 }
 
